630f246cea0ccf99e6e309e48359cf5542efe189
juancastro000 <cdyutub@gmail.com>
2025-03-09 18:05:05 +0100

    test red empty string

---
+++ src/test/java/dev/juan/transcriptor/TranscriptorTest.java
@@ -0,0 +1,16 @@
+package dev.juan.transcriptor;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import org.junit.jupiter.api.Test;
+
+public class TranscriptorTest {
+
+    @Test
+    public void testTranscribeEmptyString() {
+        TranscriptionStrategy transcription = new DnaToRnaTranscription();
+        assertThat(transcription.transcribe(""), equalTo(""));
+    }
+
+    
+
+}
